ddf1ff0c2646223a808249e05b6afaaa542e3b98
MichelAcre <michel.acre@aluno.senai.br>
2026-02-12 16:57:27 -0300

    feat: atualizar estrutura do App com novos componentes e ajustes no Perfil

--- 03-devlinks/src/App.jsx
+++ 03-devlinks/src/App.jsx
@@ -1,6 +1,9 @@
 
 import './App.css'
+import Link from './components/Link/Link'
 import Perfil from './components/Perfil/Perfil'
+import Rodape from './components/Rodape/Rodape'
+import SocialLink from './components/SocialLink/SocialLink'
 
 
 function App() {
@@ -8,15 +11,27 @@ function App() {
 return (
     <div id="App">
 
-    <Perfil />
+    <Perfil fotoPerfil={"https://placehold.co/100"}>
+      Gustavo Castello 
+    </Perfil>
 
 
       <div className='switch'>
         botão switch
       </div>
-      <div className='links'></div>
-      <div className='socialLinks'></div>
-      <div className='rodape'></div>
+      <ul>
+        <Link url={""}>Inscreva-se</Link>
+        <Link url={""}>Minha Playlist</Link>
+        <Link url={""}>Me pague um café</Link>
+        <Link url={""}>Conheça o curso DEV</Link>
+      </ul>
+
+        <SocialLink url={"https://github.com"} icon={"logo-github"}></SocialLink>
+        <SocialLink url={"https://instagram.com"} icon={"logo-instagram"}></SocialLink>
+        <SocialLink url={"https://discord.com"} icon={"logo-discord"}></SocialLink>
+        <SocialLink url={"https://youtube.com"} icon={"logo-youtube"}></SocialLink>
+
+        <Rodape>ProfCastello</Rodape>    
     </div>
   )
 }
