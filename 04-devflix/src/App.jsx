ea4d2406634d4784d62088d2ca50b67813695be6
MichelAcre <michel.acre@aluno.senai.br>
2026-02-19 11:20:37 -0300

    feat: adicionar componente Rodape e implementar busca no App

--- 04-devflix/src/App.jsx
+++ 04-devflix/src/App.jsx
@@ -1,8 +1,21 @@
 import "./App.css"
+import logo from "./assets/devflix.png"
+import lupa from "./assets/search.svg"
+import Rodape from "./components/Rodape/Rodape"
 const App = () => {
   return (
-    <div>
-      
+    <div id="App">
+      <img src={logo} alt="Logotipo do serviço de streaming DEVFLIX em destaque, com letras vermelhas e fundo preto, representando uma plataforma de entretenimento e séries online." />
+
+      <div className="search">
+      <input 
+      type="text"
+      placeholder="Pesquise por filmes e séries..." 
+      />
+      <img src={lupa} alt="Botão de ação para pesquisa" />
+      </div>
+
+      <Rodape>Michel</Rodape>
     </div>
   )
 }
