189903e99e8c460c8eac7534080f41e38b1984e4
MichelAcre <michel.acre@aluno.senai.br>
2026-04-23 13:21:49 -0300

    Alterar idioma do HTML para português e implementar componente de entrada de usuário no devChat

--- 06-devChat/client/src/components/Join/Join.jsx
+++ 06-devChat/client/src/components/Join/Join.jsx
@@ -1,9 +1,29 @@
+import { useRef } from "react";
 import style from "./Join.module.css"
 
+
+import {Input, Button} from "@mui/material"
+import io from "socket.io-client"
+
 const Join = () => {
+    const usernameRef = useRef();
+
+    const handleSubmit = async () => {
+        const username = usernameRef.current.value;
+        if (!username.trim()) return;
+
+        const socket = io.connect("http://localhost:3001");
+        socket.emit("set_username", username);
+
+    }
+
   return (
     <div>
-      Join
+      <h2>Bem-vindo ao devChat</h2>
+      <Input inputRef={usernameRef} placeholder="Nome de usuário" />
+      <Button sx={{mt:2, mb:2}} variant="contained" onClick={() => handleSubmit()}> 
+        Entrar
+      </Button>
     </div>
   )
 }
