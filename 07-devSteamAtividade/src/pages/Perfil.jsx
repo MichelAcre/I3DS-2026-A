0a23ac1ebf2a24bac8c10b082bcd5e0f1d743048
MichelAcre <michel.acre@aluno.senai.br>
2026-05-14 11:27:29 -0300

    Começado a página Perfil

---
+++ 07-devSteamAtividade/src/pages/Perfil.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from '../components/Header'
+import Rodape from '../components/Rodape'
+
+const Perfil = () => {
+  return (
+    <>
+    <Header />
+    <div>
+      
+    </div>
+    <Rodape />
+    </>
+  )
+}
+
+export default Perfil
